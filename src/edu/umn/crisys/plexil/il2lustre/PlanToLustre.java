ddff2e59e999039d13528d6a08d3432fd0f1d3a5
jbiatek <jbiatek@gmail.com>
2015-03-30 15:46:22 -0500

    New branch for Lustre output, just a stub for now

---
+++ src/edu/umn/crisys/plexil/il2lustre/PlanToLustre.java
@@ -0,0 +1,13 @@
+package edu.umn.crisys.plexil.il2lustre;
+
+import jkind.lustre.Program;
+import edu.umn.crisys.plexil.il.Plan;
+
+public class PlanToLustre {
+
+	
+	public static Program toLustre(Plan p) {
+		return null;
+	}
+
+}
