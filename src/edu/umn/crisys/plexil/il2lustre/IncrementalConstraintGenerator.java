f04408c2ea6cce847ee22e58409722b3ab700975
jbiatek <jbiatek@gmail.com>
2015-09-03 12:09:30 -0500

    Incremental now guesses where sequences are
    It's a little primitive, but it sees when a node is waiting for another
    node and assumes that it's in a sequence. To do it properly, I need
    better ability to search through the original hierarchy with node UIDs.

--- src/edu/umn/crisys/plexil/il2lustre/IncrementalConstraintGenerator.java
+++ src/edu/umn/crisys/plexil/il2lustre/IncrementalConstraintGenerator.java
@@ -4,9 +4,15 @@ import static jkind.lustre.LustreUtil.*;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import edu.umn.crisys.plexil.expr.ExprType;
+import edu.umn.crisys.plexil.expr.Expression;
+import edu.umn.crisys.plexil.expr.common.Operation;
+import edu.umn.crisys.plexil.expr.common.Operation.Operator;
+import edu.umn.crisys.plexil.expr.il.GetNodeStateExpr;
 import edu.umn.crisys.plexil.expr.il.nativebool.PlexilExprToNative;
 import edu.umn.crisys.plexil.expr.il.nativebool.PlexilExprToNative.Condition;
 import edu.umn.crisys.plexil.expr.il.vars.ILVariable;
@@ -27,7 +33,7 @@ import jkind.lustre.builders.NodeBuilder;
 public class IncrementalConstraintGenerator {
 
 	public static void go(Plan ilPlan, NodeBuilder nb, PlanToLustre p2l) {
-		
+		System.out.println("Generating incremental constraints...");
 		List<OriginalHierarchy> parentChain = 
 				findIf55(ilPlan.getOriginalHierarchy());
 		
@@ -39,19 +45,52 @@ public class IncrementalConstraintGenerator {
 		// search for that and concretize things prematurely.
 		parentChain.remove(0);
 		
+		// Add nodes in sequences too
+		System.out.println("Going through target node's ancestry");
+		List<OriginalHierarchy> actualChain = new ArrayList<>();
+		for (int i = 0; i < parentChain.size(); i++) {
+			OriginalHierarchy currentNode = parentChain.get(i);
+			// Add this one in as a goal
+			actualChain.add(currentNode);
+			System.out.println(currentNode.getUID()+" added as a goal");
+			if (i == parentChain.size() - 1) {
+				// This is the last one, no need to check for sequences
+				continue;
+			}
+			
+			
+			// Does the next node look like it's actually in a sequence?
+			OriginalHierarchy nextNode = parentChain.get(i+1);
+			Set<NodeUID> nextNodeWaitingFor = nodesThisOneIsWaitingFor(nextNode);
+			System.out.println("Next node "+nextNode.getUID()
+					+" appears to be waiting for "+nextNodeWaitingFor);
+			if ( ! nodesThisOneIsWaitingFor(nextNode).isEmpty()) {
+				// It's waiting for others. Let's go through the current
+				// node's children until we get to the next goal.
+				for (OriginalHierarchy child : currentNode.getChildren()) {
+					if (child == nextNode) break;
+					System.out.println("Adding "+child.getUID()+" as an additional goal.");
+					actualChain.add(child);
+				}
+			}
+		}
+		
 		List<Expr> failureTrackers = new ArrayList<>();
 		
-		for (OriginalHierarchy node : parentChain) {
+		for (OriginalHierarchy node : actualChain) {
 			NodeUID uid = node.getUID();
 			String id = LustreNamingConventions.getStateMapperId(uid)+"_executes";
-			// From now on, make sure it doesn't fail
-			addFailureTracker(node, p2l, nb);
-			failureTrackers.add(not(getFailureTrackerId(node)));
+			// If this was in the parent chain, from now on make sure it 
+			// doesn't fail
+			if (parentChain.contains(node)) {
+				addFailureTracker(node, p2l, nb);
+				failureTrackers.add(not(getFailureTrackerId(node)));
+			}
 			
 			List<Expr> allConditions = new ArrayList<>();
 			// This state could never be executing, right? Prove me wrong!
 			allConditions.add(nodeIsExecuting(uid, p2l));
-			// Also, make sure that nothing fails along the way.
+			// Also, make sure that nothing fails along the way
 			allConditions.addAll(failureTrackers);
 			// And that we're on a macro step boundary.
 			allConditions.add(LustreNamingConventions.MACRO_STEP_ENDED);
@@ -64,6 +103,38 @@ public class IncrementalConstraintGenerator {
 		}
 		
 	}
+	
+	private static Set<NodeUID> nodesThisOneIsWaitingFor(OriginalHierarchy node) {
+		Expression start = node.getConditions().get(PlexilExprDescription.START_CONDITION);
+		Set<NodeUID> found = new HashSet<>();
+		findNodeFinishedChecks(start, found);
+		return found;
+	}
+	
+	private static void findNodeFinishedChecks(Expression e, Set<NodeUID> found) {
+		if (e instanceof Operation) {
+			Operation oper = (Operation) e;
+			if (oper.getOperator() == Operator.EQ) {
+				if (oper.getArguments().contains(NodeState.FINISHED)) {
+					Expression other = oper.getArguments().get(0);
+					if (other.equals(NodeState.FINISHED)) {
+						// Oops
+						other = oper.getArguments().get(1);
+					}
+					
+					if (other instanceof GetNodeStateExpr) {
+						found.add(((GetNodeStateExpr) other).getNodeUid());
+					} else {
+						throw new RuntimeException("Found comparison of FINISHED to "+other);
+					}
+				}
+			}
+		}
+		for (Expression child : e.getArguments()) {
+			findNodeFinishedChecks(child, found);
+		}
+	}
+	
 	
 	private static void addFailureTracker(OriginalHierarchy node, PlanToLustre p2l,
 			NodeBuilder nb) {
@@ -163,7 +234,6 @@ public class IncrementalConstraintGenerator {
 	private static IdExpr getPlexilState(OriginalHierarchy node) {
 		return getPlexilState(node.getUID());
 	}
-	
 	
 	private static List<OriginalHierarchy> findIf55(OriginalHierarchy root) {
 		if (root.getUID().getShortName().equals("if__55")) {
