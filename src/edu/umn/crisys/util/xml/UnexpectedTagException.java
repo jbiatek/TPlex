21c44d37047f96bcd9b3233ebcb4a3f4390e4e20
Sanjai Rayadurgam <rsanjai@gmail.com>
2016-11-02 17:40:34 -0500

    PlxParser: Addition to parse variable arguments ("AnyParameters") correctly.
    PlanStateChecker:Descriptive message for cycle number when there is
    compliance mismatch
    
    UnexpectedTagException, XMLUtils: include error location in exception
    message

--- src/edu/umn/crisys/util/xml/UnexpectedTagException.java
+++ src/edu/umn/crisys/util/xml/UnexpectedTagException.java
@@ -7,11 +7,13 @@ public class UnexpectedTagException extends RuntimeException {
     private static final long serialVersionUID = -3672341180630831826L;
 
     public UnexpectedTagException(XMLEvent unexpected) {
-        super("Did not expect a "+unexpected+" tag here at "+unexpected.getLocation());
+        super(unexpected.getLocation().getLineNumber() + "," + unexpected.getLocation().getColumnNumber() +
+        		": Did not expect a "+unexpected+" tag here at "+unexpected.getLocation());
     }
     
     public UnexpectedTagException(XMLEvent unexpected, String expected) {
-        super("Expected a "+expected+" tag here, not "+unexpected+". Location: "+unexpected.getLocation());
+        super(unexpected.getLocation().getLineNumber() + "," + unexpected.getLocation().getColumnNumber() + 
+        		": Expected a "+expected+" tag here, not "+unexpected+". Location: "+unexpected.getLocation());
     }
     
 }
